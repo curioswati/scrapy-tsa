afc7458124cc8e7b75fd4cd054b2fb7071df87ea
curioswati <jaiswalswati94@gmail.com>
2017-11-21 20:21:20 +0000

    refactor classifier

--- app/utils/classifier.py
+++ app/utils/classifier.py
@@ -1,33 +1,22 @@
 import csv
-import pickle
-import sys
 
 from libsvm.svmutil import (LINEAR, svm_load_model, svm_parameter, svm_predict,
                             svm_problem, svm_save_model, svm_train)
-from utils import common, helper
+from app.utils import common, helper
 
 
 class SVMClassifier():
     """ SVM Classifier """
-    def __init__(self, data, keyword, training_datafile,
-                 classifier_dumpfile, training_required=0):
+    def __init__(self, training_datafile, classifier_dumpfile, training_required=0):
 
         self.feature_list = open('app/data/feature_list.txt').readlines()
-        self.len_tweets = len(data)
-        self.orig_tweets = self.get_uniq_data(data)
-        self.tweets = self.get_processed_tweets(self.orig_tweets)
-
-        self.results = {}
-        self.neut_count = [0] * self.len_tweets
-        self.pos_count = [0] * self.len_tweets
-        self.neg_count = [0] * self.len_tweets
         self.training_datafile = training_datafile
-
-        self.keyword = keyword
+        self.classifier_dumpfile = classifier_dumpfile
 
         # call training model
         if(training_required):
-            self.classifier = self.get_SVM_trained_classifer(training_datafile, classifier_dumpfile)
+            self.classifier = self.get_SVM_trained_classifer(
+                training_datafile, classifier_dumpfile)
         else:
             fp = open(classifier_dumpfile, 'r')
             if(fp):
@@ -59,7 +48,7 @@ class SVMClassifier():
 
     def get_SVM_trained_classifer(self, training_datafile, classifier_dumpfile):
         # read all tweets and labels
-        tweet_items = self.get_filtered_training_data(training_datafile)
+        tweet_items = common.get_filtered_training_data(training_datafile)
 
         tweets = []
         for (words, sentiment) in tweet_items:
@@ -79,37 +68,6 @@ class SVMClassifier():
         svm_save_model(classifier_dumpfile, classifier)
         return classifier
 
-    def get_filtered_training_data(self, training_datafile):
-        fp = open(training_datafile, 'rb')
-        min_count = self.get_min_count(training_datafile)
-        min_count = 40000
-        neg_count, pos_count, neut_count = 0, 0, 0
-
-        reader = csv.reader(fp, delimiter=',', quotechar='"', escapechar='\\')
-        tweetItems = []
-        count = 1
-        for row in reader:
-            processed_tweet = common.process_tweet(row[1])
-            sentiment = row[0]
-
-            if(sentiment == 'neutral'):
-                if(neut_count == int(min_count)):
-                    continue
-                neut_count += 1
-            elif(sentiment == 'positive'):
-                if(pos_count == min_count):
-                    continue
-                pos_count += 1
-            elif(sentiment == 'negative'):
-                if(neg_count == int(min_count)):
-                    continue
-                neg_count += 1
-
-            tweet_item = processed_tweet, sentiment
-            tweetItems.append(tweet_item)
-            count += 1
-        return tweetItems
-
     def get_min_count(self, training_datafile):
         fp = open(training_datafile, 'rb')
         reader = csv.reader(fp, delimiter=',', quotechar='"', escapechar='\\')
@@ -124,33 +82,44 @@ class SVMClassifier():
                 neg_count += 1
         return min(neg_count, pos_count, neut_count)
 
-    def classify(self):
-        for i in self.tweets:
-            tw = self.tweets[i]
+    def classify(self, data):
+        len_tweets = len(data)
+        results = {}
+
+        neut_count = [0] * len_tweets
+        pos_count = [0] * len_tweets
+        neg_count = [0] * len_tweets
+
+        orig_tweets = self.get_uniq_data(data)
+        tweets = self.get_processed_tweets(orig_tweets)
+
+        for i in tweets:
+            tweet = tweets[i]
             test_tweets = []
             res = {}
-            for words in tw:
+            for words in tweet:
                 words_filtered = [e.lower() for e in words.split() if(common.is_ascii(e))]
                 test_tweets.append(words_filtered)
             test_feature_vector = helper.get_SVM_feature_vector(self.feature_list, test_tweets)
             p_labels, p_accs, p_vals = svm_predict([0] * len(test_feature_vector),
                                                    test_feature_vector, self.classifier)
             count = 0
-            for t in tw:
+            for t in tweet:
                 label = p_labels[count]
                 if(label == 0):
                     label = 'positive'
-                    self.pos_count[i] += 1
+                    pos_count[i] += 1
                 elif(label == 1):
                     label = 'negative'
-                    self.neg_count[i] += 1
+                    neg_count[i] += 1
                 elif(label == 2):
                     label = 'neutral'
-                    self.neut_count[i] += 1
-                result = {'text': t, 'tweet': self.orig_tweets[i][count], 'label': label}
+                    neut_count[i] += 1
+                result = {'text': t, 'tweet': orig_tweets[i][count], 'label': label}
                 res[count] = result
                 count += 1
-            self.results[i] = res
+            results[i] = res
+        return results
 
     def write_output(self, filename, write_option='w'):
         fp = open(filename, write_option)
@@ -164,7 +133,7 @@ class SVMClassifier():
                 fp.write(write_str)
 
     def accuracy(self):
-        tweets = self.get_filtered_training_data(self.training_datafile)
+        tweets = common.get_filtered_training_data(self.training_datafile)
         test_tweets = []
         for (t, l) in tweets:
             words_filtered = [e.lower() for e in t.split() if(common.is_ascii(e))]
@@ -191,30 +160,6 @@ class SVMClassifier():
                 wrong += 1
             total += 1
             count += 1
-        self.accuracy = (float(correct)/total)*100
+        accuracy = (float(correct)/total)*100
         print 'Total = {}, Correct = {}, Wrong = {}, Accuracy = {}'.format(
-            total, correct, wrong, self.accuracy)
-
-    def train_model(test_tweets_file):
-        '''
-        This function trains the svm classifier using the training data.
-        '''
-        # get tweets from file
-        tweets_file = open(test_tweets_file)
-        tweets = pickle.load(tweets_file)
-        tweets_file.close()
-
-        training_datafile = 'app/data/full_training_dataset.csv'
-        classifier_dumpfile = 'app/data/svm_trained_model.pickle'
-        training_required = 1
-        keyword = 'scrapy'
-
-        sys.stdout.flush()
-        sc = SVMClassifier(tweets, keyword, training_datafile,
-                           classifier_dumpfile, training_required)
-        print 'Computing Accuracy'
-        sys.stdout.flush()
-        sc.classify()
-        sc.accuracy()
-        print 'Done'
-        sys.stdout.flush()
+            total, correct, wrong, accuracy)
